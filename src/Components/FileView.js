346210d88b35a0479d0d8ef89fcc76e1c5500990
Dat_Daag <arveshgosine@gmail.com>
2023-07-14 17:35:14 -0400

    Implemented feature to add FileViews

---
+++ src/Components/FileView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FileView = () => {
+  return (
+    <div className="bg-white border-2 border-black w-[250px] h-[250px]">
+      FileView
+    </div>
+  );
+};
+
+export default FileView;
--- src/WorkSpace.js
+++ src/WorkSpace.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Document, Page } from "react-pdf";
-import { AiOutlineMenu } from "react-icons/ai";
+import {
+  AiOutlineMenu,
+  AiFillPlusCircle,
+  AiFillMinusCircle,
+} from "react-icons/ai";
 
 import Test from "./Test.pdf";
+import FileView from "./Components/FileView";
 
 import { pdfjs } from "react-pdf";
 
@@ -15,32 +20,65 @@ const WorkSpace = () => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
+  const [fileViews, setFileViews] = useState([]);
+
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
     console.log(numPages);
     setPageNumber(50);
   }
 
-  console.log(Test);
+  useEffect(() => {
+    console.log(fileViews);
+  }, [fileViews]);
+
+  const addNewFileView = () => {
+    const updatedFileViews = [...fileViews];
+    let fileViewKey = updatedFileViews.length;
+
+    const newFileView = (
+      <div key={fileViewKey}>
+        <FileView />
+      </div>
+    );
+
+    updatedFileViews.push(newFileView);
+    setFileViews(updatedFileViews);
+  };
 
   return (
     <div className="bg-[#001404] w-screen h-screen">
       <div className="flex flex-col">
-        <div className=" h-10 w-auto border-b-2 border-[#39ad60] rounded-lg">
-          <div
-            className="p-1 absolute rounded-full hover:bg-[#64ed9f] hover:bg-opacity-40 hover:cursor-pointer
-                       border-b-4 border-r-2 border-[#64ed9f]"
-          >
-            <AiOutlineMenu size={25} color="#FFFFFF" />
+        <div className=" h-10 relative w-auto border-b-2 border-[#39ad60] rounded-lg">
+          <div>
+            <div
+              className="p-1 absolute rounded-full hover:bg-[#64ed9f] hover:bg-opacity-40
+                      hover:cursor-pointer border-b-4 border-r-2 border-[#64ed9f]"
+            >
+              <AiOutlineMenu size={25} color="#FFFFFF" />
+            </div>
+            <div className=" flex items-center justify-center h-10">
+              <div
+                className="p-2 text-white  hover:text-[#64ed9f] hover:cursor-pointer"
+                onClick={() => {
+                  addNewFileView();
+                }}
+              >
+                <AiFillPlusCircle size={22} />
+              </div>
+              <div
+                className="p-2 text-white hover:text-[#fa23ba] hover:cursor-pointer"
+                onClick={() => {}}
+              >
+                <AiFillMinusCircle size={22} />
+              </div>
+            </div>
           </div>
         </div>
-        <div className=" h-96 w-96">
-          <Document file={Test} onLoadSuccess={onDocumentLoadSuccess}>
-            <Page height={600} width={900} pageNumber={pageNumber} />
-          </Document>
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
+        <div className="relative">
+          {fileViews.map((fileView, index) => (
+            <div key={index}>{fileView}</div>
+          ))}
         </div>
       </div>
     </div>
